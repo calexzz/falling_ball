209fb369ffaa6941b517a5199b7b7ba6571dc09e
root <root@LAPTOP-3I9FKNT7.localdomain>
2026-05-23 10:14:23 +0800

    Added some commentary

--- projector.py
+++ projector.py
@@ -1,17 +1,22 @@
 import cv2
 import numpy as np
+
+# pip install screeninfo
 from screeninfo import get_monitors
 
+# Передаём наше изображение 
 def show_on_monitor(mask):
+    # Получаем количество и информацию о мониторах
     monitors = get_monitors
+    # Считаем, есть ли второй монитор
     if len(monitors < 2):
         print("Обнаружен лишь один монитор")
         target_monitor = monitors[0]
     else:
         target_monitor = monitors[1]
 
+    # Выводим на проектор полноэкранное изображение
     cv2.namedWindow("Mask", cv2.WND_PROP_FULLSCREEN)
     cv2.moveWindow("Mask", target_monitor.x, target_monitor.y)
     cv2.setWindowProperty("Mask", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
     cv2.imshow("Mask", mask)
